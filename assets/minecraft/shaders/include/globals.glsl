936d5a45099ca84849f2985594ed7f98150cd9b8
InventiveBot <git-bot@inventivetalent.org>
2025-11-04 18:32:50 +0100

    Create/Update assets for version 25w45a

---
+++ assets/minecraft/shaders/include/globals.glsl
@@ -0,0 +1,10 @@
+#version 330
+
+layout(std140) uniform Globals {
+    ivec3 CameraBlockPos;
+    vec3 CameraOffset;
+    vec2 ScreenSize;
+    float GlintAlpha;
+    float GameTime;
+    int MenuBlurRadius;
+};
