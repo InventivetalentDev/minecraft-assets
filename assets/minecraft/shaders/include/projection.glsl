09ad6bc70c471c4aadb4c5e4a2fa23b3bea3ca48
InventiveBot <git-bot@inventivetalent.org>
2026-06-09 18:34:27 +0200

    Create/Update assets for version 26.2-pre-5

---
+++ assets/minecraft/shaders/include/projection.glsl
@@ -0,0 +1,12 @@
+#version 330
+
+layout(std140) uniform Projection {
+    mat4 ProjMat;
+};
+
+vec4 projection_from_position(vec4 position) {
+    vec4 projection = position * 0.5;
+    projection.xy = vec2(projection.x + projection.w, projection.y + projection.w);
+    projection.zw = position.zw;
+    return projection;
+}
